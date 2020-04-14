122044f4c6a37136bfba4e3fdd2c586c493542b0
Tugume Andrew <andrewtugume2@gmail.com>
2020-04-14 23:23:27 +0300

    added web component

--- footer.js
+++
@@ -1,9 +0,0 @@
-//THIS A WEB COMPONENT
-class Footer extends HTMLElement{
-constructor(){
-    super();
-    this.innerHTML=`I am a web component`
-}
-}
-
-window.customElements.define('my-footer',Footer)
